92680ca308bae18a65687bc2ae3a33d33391ea47
Mykolas Juraitis <mykolas@engineeredarts.co.uk>
2023-12-17 20:36:04 +0000

    feat: remember URL and user

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
+const urlElement = document.getElementById("url")
+urlElement.value = localStorage.getItem("url");
+urlElement.onblur = () => {
+	localStorage.setItem("url", urlElement.value);
+}
+
+const userElement = document.getElementById("user")
+userElement.value = localStorage.getItem("user");
+userElement.onblur = () => {
+	localStorage.setItem("user", userElement.value);
+}
+
+const passwordElement = document.getElementById("password");
+
 document.getElementById("connect").onclick = () => {
-	const url = document.getElementById("url").value;
-	const user = document.getElementById("user").value;
-	const password = document.getElementById("password").value;
+	const url = urlElement.value;
+	const user = userElement.value;
+	const password = passwordElement.value;
 	const ws = new WebSocket(`ws://${user}:${password}@${url}`);
 };
